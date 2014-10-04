e9ee32cc6dad6b4bf51c2e036f0172616d6c62f7
Hiroshi Saito <hiroshi3110@gmail.com>
2014-10-04 09:27:15 +0900

    Initial Commit

---
+++ TrayiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrayiOS
+//
+//  Created by hiroshi on 2014/10/04.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrayiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrayiOS
+//
+//  Created by hiroshi on 2014/10/04.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
